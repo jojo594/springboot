5fa8b5b18fa5830f4184079fb0157b2373645298
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2025-05-13 00:16:32 +0300

    discovery api ve genre

---
+++ src/main/java/com/proje/login/DTO/GenreDTO.java
@@ -0,0 +1,13 @@
+package com.proje.login.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class GenreDTO {
+    private Integer id;
+    private String name;
+} 
---
+++ src/main/java/com/proje/login/DTO/GenreListDTO.java
@@ -0,0 +1,14 @@
+package com.proje.login.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class GenreListDTO {
+    private List<GenreDTO> genres;
+} 
